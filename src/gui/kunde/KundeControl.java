580762ce2f3e6b25db94ed91f085c25c9e8eed24
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-11-25 18:30:45 +0100

    Create KundeControl.java

---
+++ src/gui/kunde/KundeControl.java
@@ -0,0 +1,51 @@
+package gui.kunde;
+
+import business.kunde.Kunde;
+import business.kunde.KundeModel;
+import gui.grundriss.GrundrissControl;
+import javax.swing.JOptionPane;
+
+/**
+ * Klasse, welche das Grundfenster mit den Kundendaten kontrolliert.
+ */
+public class KundeControl {
+       
+    // das View-Objekt des Grundfensters
+	private KundeView kundeView;
+    // das Model-Objekt des Grundfensters
+    private KundeModel kundeModel;
+    // das GrundrissControl-Objekt zum Kunden
+    private GrundrissControl grundrissControl;
+    
+    /**
+	 * erzeugt ein das View-Objekt und Model-Objekt zum Grundfenster und
+	 * oeffnet das View.
+	 */
+    public KundeControl() { 
+        this.kundeModel = KundeModel.getInstance(); 
+        this.kundeView = new KundeView(this, kundeModel);
+    }
+    
+    /*
+     * erstellt, falls nicht vorhanden, ein Grundriss-Control-Objekt.
+     * Das View wird sichtbar gemacht.
+     */
+    public void oeffneGrundrissControl(){
+    	if (this.grundrissControl == null){
+    		this.grundrissControl = new GrundrissControl(kundeModel);
+    	}
+    	this.grundrissControl.oeffneGrundrissView();
+    }
+    
+    public void speichereKunden(Kunde kunde){
+      	try{
+    		kundeModel.speichereKunden(kunde);
+    	}
+    	catch(Exception exc){
+    		exc.printStackTrace();
+       		JOptionPane.showMessageDialog(this.kundeView, 
+    			"DB-Fehler: Der Kunde konnte nicht gespeichert werden.");
+    	}
+    }
+  
+}
