3be6722bc0611c914ed470726d1264addb3146ff
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-11-25 18:28:09 +0100

    Add files via upload

---
+++ src/gui/grundriss/GrundrissControl.java
@@ -0,0 +1,35 @@
+package gui.grundriss;
+
+import business.kunde.KundeModel;
+
+/**
+ * Klasse, welche das Fenster mit den Sonderwuenschen zu den Grundriss-Varianten
+ * kontrolliert.
+ */
+public final class GrundrissControl {
+	
+	// das View-Objekt des Grundriss-Fensters
+	private GrundrissView grundrissView;
+
+	/**
+	 * erzeugt ein das View-Objekt und Model-Objekt zum Grundriss-Fenster und
+	 * oeffnet das View.
+	 */
+	public GrundrissControl(KundeModel kundeModel){  
+	    this.grundrissView = new GrundrissView(this, kundeModel);
+	}
+	    
+	/**
+	 * macht das GrundrissView-Objekt sichtbar.
+	 */
+	public void oeffneGrundrissView(){ 
+		this.grundrissView.setVisible(true);
+	}
+
+	public void leseGrundrissSonderwuensche(){
+    } 
+	
+	public boolean pruefeKonstellationSonderwuensche(int[] ausgewaehlteSw){
+		return true;
+	}
+}
