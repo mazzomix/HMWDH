875a6f47d0844de1f37c0e0e3f00f347fd5599b3
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-11-29 14:06:54 +0100

    GrundrissModel mit Preisberechnung

---
+++ src/Gui/Grundriss/GrundrissModel.java
@@ -0,0 +1,33 @@
+package gui.grundriss;
+
+
+public class GrundrissModel  {
+	private int _preis = 0;
+	private boolean _auswahl; 
+	
+	public void gesamtpreisBerechnen (int[] preise) { 
+		for(int i = 0; i<preise.length; i++) {
+			this._preis += preise[i]; 
+		}
+		
+	}
+	
+	public int getPreis() {
+		return _preis;
+	}
+	
+	public void checkAuswahl(int[] auswahl) {
+		//2.2 geht nur wenn 2.1 auch ausgesucht wurde
+		//2.4 geht nur , wenn DG vorhanden ist. Standardgemäß ist die Treppe von dem Raum im DG nur durch eine Brüstung getrennt.
+		//2.5 genut nur, wenn DG vorhanden ist.Die Vorrichtung eines Bades beinhaltet die Verlegung von Leitungen für Warmwasser, Kaltwasser und Abwasser.
+		//2.6 get nur, wenn DG vorhanden ist und 2.5 ausgesucht wurde. Die Ausführung eines Bades beinhaltet den Erbau einer Dusche, eines WC’s und eines Waschbeckens.
+		this._auswahl = true;
+	}
+	public boolean getAuswahl() {
+		return _auswahl;
+	}
+	
+	public void resetPreis() {
+		_preis = 0;
+	}
+}
