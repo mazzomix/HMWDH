b136fd9ed65016f156e912d2b787be5b4e385bf5
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-11-29 15:07:07 +0100

    Update GrundrissModel.java

--- src/Gui/Grundriss/GrundrissModel.java
+++ src/Gui/Grundriss/GrundrissModel.java
@@ -3,7 +3,7 @@ package gui.grundriss;
 
 public class GrundrissModel  {
 	private int _preis = 0;
-	private boolean _auswahl; 
+	private boolean _auswahl = true; 
 	
 	public void gesamtpreisBerechnen (int[] preise) { 
 		for(int i = 0; i<preise.length; i++) {
@@ -16,12 +16,27 @@ public class GrundrissModel  {
 		return _preis;
 	}
 	
-	public void checkAuswahl(int[] auswahl) {
+	public void checkAuswahl(int[] auswahl, int hausnummer) {
+		
 		//2.2 geht nur wenn 2.1 auch ausgesucht wurde
-		//2.4 geht nur , wenn DG vorhanden ist. Standardgemäß ist die Treppe von dem Raum im DG nur durch eine Brüstung getrennt.
-		//2.5 genut nur, wenn DG vorhanden ist.Die Vorrichtung eines Bades beinhaltet die Verlegung von Leitungen für Warmwasser, Kaltwasser und Abwasser.
-		//2.6 get nur, wenn DG vorhanden ist und 2.5 ausgesucht wurde. Die Ausführung eines Bades beinhaltet den Erbau einer Dusche, eines WC’s und eines Waschbeckens.
-		this._auswahl = true;
+		//2.4 geht nur , wenn DG vorhanden ist. StandardgemÃ¤ÃŸ ist die Treppe von dem Raum im DG nur durch eine BrÃ¼stung getrennt.
+		//2.5 genut nur, wenn DG vorhanden ist.Die Vorrichtung eines Bades beinhaltet die Verlegung von Leitungen fÃ¼r Warmwasser, Kaltwasser und Abwasser.
+		//2.6 get nur, wenn DG vorhanden ist und 2.5 ausgesucht wurde. Die AusfÃ¼hrung eines Bades beinhaltet den Erbau einer Dusche, eines WCâ€™s und eines Waschbeckens.
+	
+		if(auswahl[1]!=0 && auswahl[0] == 0) {
+			this._auswahl = false;
+		}
+		if(auswahl[3] != 0 && (hausnummer == 1 || hausnummer == 6 || hausnummer == 7|| hausnummer == 14 || hausnummer == 15|| hausnummer == 24)) {
+			this._auswahl = false;
+		}
+		if(auswahl[4] != 0 && (hausnummer == 1 || hausnummer == 6 || hausnummer == 7|| hausnummer == 14 || hausnummer == 15|| hausnummer == 24)){
+			this._auswahl = false;
+		}
+		if(auswahl[5]!= 0 && ((hausnummer == 1 || hausnummer == 6 || hausnummer == 7|| hausnummer == 14 || hausnummer == 15|| hausnummer == 24) || auswahl[4] == 0)) {
+			this._auswahl = false;
+		}
+		
+		
 	}
 	public boolean getAuswahl() {
 		return _auswahl;
