e53418a52d2aeeeffc6496ca94ad52feda898c04
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-11-29 15:21:08 +0100

    Update GrundrissView.java

--- src/Gui/Grundriss/GrundrissView.java
+++ src/Gui/Grundriss/GrundrissView.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-
+import Gui.Basis.BasisView;
 /**
  * Klasse, welche das Fenster mit den Sonderwuenschen zu
  * den Grundrissvarianten bereitstellt.
