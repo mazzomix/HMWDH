6d2c5704d0c1e3fd9fe29a4731cf7a3141557728
Alonfire <alonfire@gmx.de>
2018-12-13 16:26:39 +0100

    commit

---
+++ src/Gui/Sanitaerinstallation/SanitaerinstallationModel.java
@@ -0,0 +1,32 @@
+package Gui.Sanitaerinstallation;
+
+public class SanitaerinstallationModel {
+
+        private int preis = 0;
+        private boolean auswahl;
+
+
+      public void gesamtpreisBerechnen (int[] preise) {
+        for(int i = 0; i<preise.length; i++) {
+            this.preis += preise[i];
+        }
+
+       }
+
+
+    public boolean getAuswahl() {
+        return auswahl;
+    }
+
+    public void resetAuswahl() {
+        auswahl = true;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public void resetPreis() { preis = 0;}
+
+}
+
