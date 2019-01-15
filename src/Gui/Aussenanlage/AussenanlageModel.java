1c94e9fdf498e7fd8fb292c9b79cb8f5afad3e58
YlenNads <39728515+YlenNads@users.noreply.github.com>
2019-01-15 15:14:14 +0100

    Update AussenanlageModel.java

--- src/Gui/Aussenanlage/AussenanlageModel.java
+++ src/Gui/Aussenanlage/AussenanlageModel.java
@@ -3,7 +3,7 @@ package Gui.Aussenanlage;
 import Business.KundeModel;
 
 public class AussenanlageModel {
-    private int _preis = 0;
+    private double _preis = 0;
     private boolean _auswahl = true;
     private KundeModel kunde = KundeModel.getInstance();
 
@@ -14,10 +14,36 @@ public class AussenanlageModel {
 
     }
 
-    public int getPreis() {
+    public void checkAuswahl(double[] auswahl) {
+
+        if(auswahl[2] != 0 && (kunde.getKunde().getHausnummer().getHaustyp().getDachgeschoss() != 1)) {
+            this._auswahl = false;
+        }
+        if(auswahl[5] != 0 && (kunde.getKunde().getHausnummer().getHaustyp().getDachgeschoss() != 1)){
+            this._auswahl = false;
+        }
+        if(auswahl[4]!= 0 && ((kunde.getKunde().getHausnummer().getHaustyp().getDachgeschoss() != 1) || auswahl[2] == 0)) {
+            this._auswahl = false;
+        }
+        if(auswahl[6] != 0 && ((kunde.getKunde().getHausnummer().getHaustyp().getDachgeschoss() != 1) || auswahl[5] == 0)){
+            this._auswahl = false;
+        }
+    }
+
+    public boolean getAuswahl(){
+        return  _auswahl;
+    }
+
+    public double getPreis() {
         return _preis;
     }
 
+    public void resetPreis() {
+        _preis = 0;
+    }
 
+    public  void resetAuswahl(){
+        _auswahl = true;
+    }
 
 }
