9fefffbd8aad766b13ecef21a91fd1d5dd050af3
YlenNads <39728515+YlenNads@users.noreply.github.com>
2019-01-15 15:12:33 +0100

    Update AussenanlageView.java

--- src/Gui/Aussenanlage/AussenanlageView.java
+++ src/Gui/Aussenanlage/AussenanlageView.java
@@ -10,8 +10,11 @@ import java.util.Set;
 
 public class AussenanlageView extends  BasisView {
     public static final long serialVersionUID = 1L;
-
+    private double[] auswahl = new double[7];
     private AussenanlageControl aussenanlageControl;
+
+    private List<SonderwuenscheAussenanlagen> sonderwuenscheAussenanlagen = db.holeSonderwuenscheAussenanlagen();
+
 
     private JLabel lblEGAbstellraumTerrasse = new JLabel("Abstellraum auf der Terrasse des EG");
     private JTextField txtPreisEGAbstellraumTerrasse = new JTextField("3590");
@@ -57,7 +60,7 @@ public class AussenanlageView extends  BasisView {
         this.setTitle("Sonderwünsche zu Aussenanlage-Varianten");
         this.aussenanlageControl.setWuensche(db.holeSonderwuenscheAussenanlagen());
         this.initKomponenten();
-        this.initListener();
+        this.iniListener();
         this.leseAussenanlageSonderwuensche();
     }
 
@@ -106,32 +109,42 @@ public class AussenanlageView extends  BasisView {
         super.getPnlSonderwunsch().add(chckBxEGElektrischeMarkise);
         chckBxEGElektrischeMarkise.setBounds(470, 125, 25, 25);
 
+         super.getPnlSonderwunsch().add(lblDGElektrischeMarkise);
+        lblDGElektrischeMarkise.setBounds(10, 150, 350, 25);
+        super.getPnlSonderwunsch().add(txtPreisDGElektrischeMarkise);
+        txtPreisDGElektrischeMarkise.setBounds(350, 150, 80, 25);
+        txtPreisDGElektrischeMarkise.setEditable(false);
+        super.getPnlSonderwunsch().add(lblDGElektrischeMarkiseEuro);
+        lblDGElektrischeMarkiseEuro.setBounds(440, 150, 50, 25);
+        super.getPnlSonderwunsch().add(chckBxDGElektrischeMarkise);
+        chckBxDGElektrischeMarkise.setBounds(470, 150, 25, 25);
+
         super.getPnlSonderwunsch().add(lblSektionaltor);
-        lblSektionaltor.setBounds(10, 150, 350, 25);
+        lblSektionaltor.setBounds(10, 175, 350, 25);
         super.getPnlSonderwunsch().add(txtPreisSektionaltor);
-        txtPreisSektionaltor.setBounds(350, 150, 80, 25);
+        txtPreisSektionaltor.setBounds(350, 175, 80, 25);
         txtPreisSektionaltor.setEditable(false);
         super.getPnlSonderwunsch().add(lblSektionaltorEuro);
-        lblSektionaltorEuro.setBounds(440, 150, 50, 25);
+        lblSektionaltorEuro.setBounds(440, 175, 50, 25);
         super.getPnlSonderwunsch().add(chckBxSektionaltor);
-        chckBxSektionaltor.setBounds(470, 150, 25, 25);
+        chckBxSektionaltor.setBounds(470, 175, 25, 25);
 
         super.getPnlSonderwunsch().add(lblEleAntriebGaragentor);
-        lblEleAntriebGaragentor.setBounds(10, 175, 350, 25);
+        lblEleAntriebGaragentor.setBounds(10, 200, 350, 25);
         super.getPnlSonderwunsch().add(txtPreisEleAntriebGaragentor);
-        txtPreisEleAntriebGaragentor.setBounds(350, 175, 80, 25);
+        txtPreisEleAntriebGaragentor.setBounds(350, 200, 80, 25);
         txtPreisEleAntriebGaragentor.setEditable(false);
         super.getPnlSonderwunsch().add(lblEleAntriebGaragentorEuro);
-        lblEleAntriebGaragentorEuro.setBounds(440, 175, 50, 25);
+        lblEleAntriebGaragentorEuro.setBounds(440, 200, 50, 25);
         super.getPnlSonderwunsch().add(chckBxEleAntriebGaragentor);
-        chckBxEleAntriebGaragentor.setBounds(470, 175, 25, 25);
+        chckBxEleAntriebGaragentor.setBounds(470, 200, 25, 25);
 
-         super.getPnlSonderwunsch().add(lblGesamtpreis);
-    	lblGesamtpreis.setBounds(10, 225, 350, 25);
+        super.getPnlSonderwunsch().add(lblGesamtpreis);
+    	lblGesamtpreis.setBounds(10, 250, 350, 25);
     	super.getPnlSonderwunsch().add(txtGesamtpreis);
-    	txtGesamtpreis.setBounds(350, 225, 150, 25);
+    	txtGesamtpreis.setBounds(350, 250, 150, 25);
     	txtGesamtpreis.setEditable(false);
-
+        
         lblEGAbstellraumTerrasse.setText(aussenanlageControl.getWuensche().get(0).getWunsch());
         txtPreisEGAbstellraumTerrasse.setText(String.valueOf(aussenanlageControl.getWuensche().get(0).getPreis()));
         lblEGVorbereitungEleAntriebe.setText(aussenanlageControl.getWuensche().get(1).getWunsch());
@@ -153,6 +166,116 @@ public class AussenanlageView extends  BasisView {
     protected void iniListener() {
         super.initListener();
 
+        protected void inisListener() {
+        super.initListener();
+
+        chckBxEGAbstellraumTerrasse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(0));
+                    auswahl[0] = sonderwuenscheAussenanlagen.get(0).getPreis();
+
+
+                }else {
+                    auswahl[0] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(0).getId());
+                }
+
+            }
+        });
+        chckBxVorbereitungEleAntriebe.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(1));
+                    auswahl[1] = sonderwuenscheAussenanlagen.get(1).getPreis();
+
+                }else {
+                    auswahl[1] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(1).getId());
+                }
+
+            }
+        });
+        chckBxDGVorbereitungEleAntriebe.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(2));
+                    auswahl[2] = sonderwuenscheAussenanlagen.get(2).getPreis();
+
+                }else {
+                    auswahl[2] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(2).getId());
+                }
+
+            }
+        });
+        chckBxEGElektrischeMarkise.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(3));
+                    auswahl[3] = sonderwuenscheAussenanlagen.get(3).getPreis();
+
+                }else {
+                    auswahl[3] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(3).getId());
+                }
+
+            }
+        });
+        chckBxDGElektrischeMarkise.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(4));
+                    auswahl[4] = sonderwuenscheAussenanlagen.get(4).getPreis();
+
+                }else {
+                    auswahl[4] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(4).getId());
+                }
+
+            }
+        });
+        chckBxEleAntriebGaragentor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(5));
+                    auswahl[5] = sonderwuenscheAussenanlagen.get(5).getPreis();
+
+                }else {
+                    auswahl[5] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(5).getId());
+                }
+
+            }
+        });
+        chckBxSektionaltor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AbstractButton abBttn = (AbstractButton)actionEvent.getSource();
+                if(abBttn.getModel().isSelected()) {
+                    aussenanlageControl.addAusgewaehltenWuensch(sonderwuenscheAussenanlagen.get(6));
+                    auswahl[6] = sonderwuenscheAussenanlagen.get(6).getPreis();
+
+                }else {
+                    auswahl[6] = 0;
+                    aussenanlageControl.removeAusgewaehltenWunsch(aussenanlageControl.getWuensche().get(6).getId());
+                }
+
+            }
+        });
+        
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowActivated(WindowEvent e) {
@@ -208,15 +331,29 @@ public class AussenanlageView extends  BasisView {
     }
 
 
-    private void leseAussenanlageSonderwuensche(){this.aussenanlageControl.leseAussenanlageSonderwuensche();}
+   private void leseAussenanlageSonderwuensche(){this.aussenanlageControl.leseAussenanlageSonderwuensche();}
 
     protected  void berechneUndZeigePreisSonderwuensche(){
-        double preis = aussenanlageControl.berechnePreis();
-        txtGesamtpreis.setText("" + Double.toString(preis) + " €");
+        if(aussenanlageControl.pruefeKonstellationSonderwuensche(auswahl)) {
+            aussenanlageControl.zeigePreisSonderwuensche(auswahl);
+
+        }else {
+            aussenanlageControl.zeigeFehlerSonderwunsch();
+        }
+
+
+
+        //double preis = aussenanlageControl.berechnePreis();
+        //txtGesamtpreis.setText("" + Double.toString(preis) + " €");
     }
 
     protected  void speichereSonderwuensche(){
         kunde.getKunde().setSonderwuenscheAussenanlagen(aussenanlageControl.getAusgewaehlteWuensche());
         db.speichereKunden(kunde.getKunde());
+    }
+
+    protected JTextField getTxtGesamtpreis() {
+        return this.txtGesamtpreis;
+
     }
 }
