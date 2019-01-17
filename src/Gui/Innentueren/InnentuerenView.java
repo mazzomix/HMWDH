cd31d6b77d9ccbbdb5bacdbc791fb3e1834e1bcb
Ceadis <alex.pagel@hotmail.de>
2019-01-17 23:23:55 +0100

    Update InnentuerenView.java

--- src/Gui/Innentueren/InnentuerenView.java
+++ src/Gui/Innentueren/InnentuerenView.java
@@ -27,7 +27,8 @@ public class InnentuerenView extends BasisView {
 
     public static final long serialVersionUID = 1L;
     private double[] auswahl = new double[10];
-    private int[] anzahl = new int[10];
+    private int[] anzahl = new int[9];
+    private int[] anzahl1 = new int[4];
 
     private InnentuerenControl innentuerenControl;
     // Definition der grafischen Oberflaeche
@@ -54,6 +55,7 @@ public class InnentuerenView extends BasisView {
 
     private JLabel lblKG = new JLabel("KG: ");
     private JLabel lblEG = new JLabel("EG: ");
+    private JLabel lblOG = new JLabel("OG: ");
     private JLabel lblDG = new JLabel("DG: ");
     private JLabel lblGG = new JLabel("Garage");
     
@@ -62,7 +64,10 @@ public class InnentuerenView extends BasisView {
     private JTextField txtKGMax = new JTextField("0");   
     private JTextField txtMaengeEGKlar = new JTextField("0");
     private JTextField txtMaengeEGMilch = new JTextField("0");
-    private JTextField txtEGMax = new JTextField("0"); 
+    private JTextField txtEGMax = new JTextField("0");
+    private JTextField txtMaengeOGKlar = new JTextField("0");
+    private JTextField txtMaengeOGMilch = new JTextField("0");
+    private JTextField txtOGMax = new JTextField("0");
     private JTextField txtMaengeDGKlar = new JTextField("0");
     private JTextField txtMaengeDGMilch = new JTextField("0");
     private JTextField txtDGMax = new JTextField("0");
@@ -101,6 +106,15 @@ public class InnentuerenView extends BasisView {
         anzahl[5] = 0;
         auswahl[6] = 0;
         anzahl[6] = 0;
+        auswahl[7] = 0;
+        anzahl[7] = 0;
+        auswahl[8] = 0;
+        anzahl[8] = 0;
+
+        anzahl1[0] = 0;
+        anzahl1[1] = 0;
+        anzahl1[2] = 0;
+        anzahl1[3] = 0;
         
         //ALT
         super.getPnlSonderwunsch().add(lblKlarGlasInnentuer);
@@ -139,10 +153,12 @@ public class InnentuerenView extends BasisView {
         lblKG.setBounds(10, 175, 50, 25);
         super.getPnlSonderwunsch().add(lblEG);
         lblEG.setBounds(10, 200, 50, 25);
-        super.getPnlSonderwunsch().add(lblDG);
+        super.getPnlSonderwunsch().add(lblOG);
         lblDG.setBounds(10, 225, 50, 25);
+        super.getPnlSonderwunsch().add(lblDG);
+        lblDG.setBounds(10, 250, 50, 25);
         super.getPnlSonderwunsch().add(lblGG);
-        lblGG.setBounds(10, 250, 50, 25);    
+        lblGG.setBounds(10, 275, 50, 25);
                  
         super.getPnlSonderwunsch().add(lblKlar);
         lblKlar.setBounds(185, 150, 150, 25);
@@ -156,6 +172,7 @@ public class InnentuerenView extends BasisView {
       
         super.getPnlSonderwunsch().add(txtKGMax);
         txtKGMax.setBounds(100, 175, 40, 25);
+        txtKGMax.setText(String.valueOf(innentuerenControl.initMaengeKeller(anzahl1[0])));
         txtKGMax.setEditable(false);
      
         
@@ -164,32 +181,44 @@ public class InnentuerenView extends BasisView {
      
         super.getPnlSonderwunsch().add(txtMaengeEGMilch);
         txtMaengeEGMilch.setBounds(260, 200, 40, 25);
-       
-      
+
         super.getPnlSonderwunsch().add(txtEGMax);
         txtEGMax.setBounds(100, 200, 40, 25);
+        txtEGMax.setText(String.valueOf(innentuerenControl.initMaengeEG(anzahl1[1])));
         txtEGMax.setEditable(false);
+
+        super.getPnlSonderwunsch().add(txtMaengeOGKlar);
+        txtMaengeEGKlar.setBounds(185, 225, 40, 25);
+
+        super.getPnlSonderwunsch().add(txtMaengeOGMilch);
+        txtMaengeEGMilch.setBounds(260, 225, 40, 25);
+
+        super.getPnlSonderwunsch().add(txtOGMax);
+        txtOGMax.setBounds(100, 225, 40, 25);
+        txtOGMax.setText(String.valueOf(innentuerenControl.initMaengeOG(anzahl1[2])));
+        txtOGMax.setEditable(false);
         
         super.getPnlSonderwunsch().add(txtMaengeDGKlar);
-        txtMaengeDGKlar.setBounds(185, 225, 40, 25);
+        txtMaengeDGKlar.setBounds(185, 250, 40, 25);
        
         super.getPnlSonderwunsch().add(txtMaengeDGMilch);
-        txtMaengeDGMilch.setBounds(260, 225, 40, 25);
-       
+        txtMaengeDGMilch.setBounds(260, 250, 40, 25);
+
         super.getPnlSonderwunsch().add(txtDGMax);
-        txtDGMax.setBounds(100, 225, 40, 25);
+        txtDGMax.setBounds(100, 250, 40, 25);
+        txtDGMax.setText(String.valueOf(innentuerenControl.initMaengeDG(anzahl1[3])));
         txtDGMax.setEditable(false);
         
         super.getPnlSonderwunsch().add(txtGGMax);
-        txtGGMax.setBounds(100, 250, 40, 25);
+        txtGGMax.setBounds(100, 275, 40, 25);
         txtGGMax.setEditable(false);
       
         //ALT
 
         super.getPnlSonderwunsch().add(lblGesamtpreis);
-        lblGesamtpreis.setBounds(10, 280, 350, 25);
+        lblGesamtpreis.setBounds(160, 275, 350, 25);
         super.getPnlSonderwunsch().add(txtGesamtpreis);
-        txtGesamtpreis.setBounds(100, 280, 150, 25);
+        txtGesamtpreis.setBounds(400, 275, 150, 25);
         txtGesamtpreis.setEditable(false);
         
         this.lblKlarGlasInnentuer.setText(innentuerenControl.getWuensche().get(0).getWunsch());
@@ -199,7 +228,8 @@ public class InnentuerenView extends BasisView {
         this.txtPreisKlarGlasInnentuer.setText(String.valueOf(innentuerenControl.getWuensche().get(0).getPreis()));
         this.txtPreisMilchglasInnentuer.setText(String.valueOf(innentuerenControl.getWuensche().get(1).getPreis()));
         this.txtPreisGaragentuerHolz.setText(String.valueOf(innentuerenControl.getWuensche().get(2).getPreis()));
-        
+
+
         
     }
 
@@ -222,6 +252,10 @@ public class InnentuerenView extends BasisView {
                     auswahl[2] = innentuerenControl.getWuensche().get(0).getPreis();
                     innentuerenControl.addAusgewaehltenWuensch(innentuerenControl.getWuensche().get(0));
                     anzahl[2] = Integer.parseInt(txtMaengeDGKlar.getText());
+
+                    auswahl[7] = innentuerenControl.getWuensche().get(0).getPreis();
+                    innentuerenControl.addAusgewaehltenWuensch(innentuerenControl.getWuensche().get(0));
+                    anzahl[7] = Integer.parseInt(txtMaengeOGKlar.getText());
                     
                 } else {
                 	
@@ -235,7 +269,11 @@ public class InnentuerenView extends BasisView {
                     
                     auswahl[2] = 0;
                     innentuerenControl.removeAusgewaehltenWunsch(innentuerenControl.getWuensche().get(0).getId());
-                    anzahl[2] = 0;     
+                    anzahl[2] = 0;
+
+                    auswahl[7] = 0;
+                    innentuerenControl.removeAusgewaehltenWunsch(innentuerenControl.getWuensche().get(0).getId());
+                    anzahl[7] = 0;
                 }
 
             }
@@ -258,6 +296,10 @@ public class InnentuerenView extends BasisView {
                     auswahl[5] = innentuerenControl.getWuensche().get(1).getPreis();
                     innentuerenControl.addAusgewaehltenWuensch(innentuerenControl.getWuensche().get(1));
                     anzahl[5] = Integer.parseInt(txtMaengeDGMilch.getText());
+
+                    auswahl[8] = innentuerenControl.getWuensche().get(1).getPreis();
+                    innentuerenControl.addAusgewaehltenWuensch(innentuerenControl.getWuensche().get(1));
+                    anzahl[8] = Integer.parseInt(txtMaengeOGMilch.getText());
                     
                 } else {
                 	
@@ -271,7 +313,11 @@ public class InnentuerenView extends BasisView {
                     
                     auswahl[5] = 0;
                     innentuerenControl.removeAusgewaehltenWunsch(innentuerenControl.getWuensche().get(0).getId());
-                    anzahl[5] = 0; 
+                    anzahl[5] = 0;
+
+                    auswahl[8] = 0;
+                    innentuerenControl.removeAusgewaehltenWunsch(innentuerenControl.getWuensche().get(0).getId());
+                    anzahl[8] = 0;
                 }
 
             }
@@ -338,6 +384,61 @@ public class InnentuerenView extends BasisView {
         
     }
 
+    private void checkeMaenge()
+    {
+        //4.1 Klarglas >1 und kleiner gesamt
+        if(((anzahl[0])<1)||((anzahl[0])>anzahl1[0]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[1])<1)||((anzahl[1])>anzahl1[1]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[2])<1)||((anzahl[2])>anzahl1[2]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[7])<1)||((anzahl[7])>anzahl1[3]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        //4.2 Milchglas >1 und kleiner gesamt
+        if(((anzahl[3])<1)||((anzahl[3])>anzahl1[0]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[4])<1)||((anzahl[4])>anzahl1[1]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[5])<1)||((anzahl[5])>anzahl1[2]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if(((anzahl[6])<1)||((anzahl[6])>anzahl1[3]))
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        //4.1 + 4.2 < gesamt
+        if((anzahl[0]+anzahl[1])>anzahl1[0])
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if((anzahl[1]+anzahl[4])>anzahl1[1])
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if((anzahl[2]+anzahl[5])>anzahl1[2])
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+        if((anzahl[7]+anzahl[8])>anzahl1[3])
+        {
+            innentuerenControl.zeigeFehlerSonderwunsch();
+        }
+    }
+
     @Override
     protected void berechneUndZeigePreisSonderwuensche() {
     	
@@ -347,6 +448,7 @@ public class InnentuerenView extends BasisView {
         } else {
             innentuerenControl.zeigeFehlerSonderwunsch();
         }
+        checkeMaenge();
     }
 
     @Override
